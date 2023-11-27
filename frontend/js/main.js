ac4d74ab594173bc19a2eba0721c665cad031352
DeepChirp <1434323966@qq.com>
2023-11-27 23:37:08 +0800

    style: change console log

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -16,7 +16,12 @@ $(function () {
                 // 从加载的内容中提取 .main-content 的内容
                 var mainContent = $data.filter('.main-content').add($data.find('.main-content')).html();
 
-                console.log(mainContent);  // 输出提取到的内容
+                // 检查提取的内容是否为空
+                if (mainContent) {
+                    console.log('Fetch content successful');
+                } else {
+                    console.log('Fetch content failed');
+                }
 
                 // 将提取的内容插入到当前页面的 .main-content 元素中
                 $('.main-content').html(mainContent);
