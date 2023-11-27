894b00cf371731397f1832ba98807ae48b3e269e
DeepChirp <1434323966@qq.com>
2023-11-27 22:39:42 +0800

    pref: optimize navigation bar

---
+++ frontend/js/main.js
@@ -0,0 +1,21 @@
+// 顶端导航栏
+$(function () {
+    $("#navbar").load("navbar.html", function () {
+        // 绑定导航栏按钮的点击事件
+        $('.navbar-button').click(function (e) {
+            e.preventDefault();  // 阻止默认的页面跳转行为
+
+            // 获取要加载的页面的 URL
+            var url = $(this).attr('href');
+
+            // 使用 AJAX 加载页面内容
+            $.get(url, function (data) {
+                // 将加载的内容插入到 .content 元素中
+                $('.content').html(data);
+            });
+
+            // 改变地址栏的 URL
+            history.pushState(null, null, url);
+        });
+    });
+});
