91720da05a7351a785c66d5c72508aecffd4e82b
ReedThree <saite@buaa.edu.cn>
2023-11-28 17:55:54 +0800

    Frontend: integrate base.js into main.js

--- frontend/js/base.js
+++
@@ -1,9 +0,0 @@
-// Script necessary for every page
-
-// Do not hardcode request URL in js file.
-// A example for API: /scores/basic_info/by_class/<int:class_id>/exam/<int:exam_id>
-// url = `${protocolPrefix}${host}/api/scores/basic_info/by_class/${class_id}/exam/${exam_id}`;
-// fetch(url).then(() => {...});
-const protocolPrefix = window.location.protocol + "//";
-const host = window.location.host;
-
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,6 +2,15 @@
 let navbarHtml = null;
 let footerHtml = null;
 
+// Script necessary for every page
+
+// Do not hardcode request URL in js file.
+// A example for API: /scores/basic_info/by_class/<int:class_id>/exam/<int:exam_id>
+// url = `${protocolPrefix}${host}/api/scores/basic_info/by_class/${class_id}/exam/${exam_id}`;
+// fetch(url).then(() => {...});
+const protocolPrefix = window.location.protocol + "//";
+const host = window.location.host;
+
 // 避免重复加载导航栏和页脚
 $(function () {
     if (navbarHtml === null) {
