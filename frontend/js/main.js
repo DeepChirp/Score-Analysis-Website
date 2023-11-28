2a666e5a9bfe7f13019307cfc8a1e117b2202cb5
DeepChirp <1434323966@qq.com>
2023-11-28 12:56:35 +0800

    fix: Blinking when switching pages

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,11 +2,12 @@
 let navbarHtml = null;
 let footerHtml = null;
 
-// 避免重复获取导航栏和页脚的 HTML 内容
+// 避免重复加载导航栏和页脚
 $(function () {
     if (navbarHtml === null) {
-        $("#navbar").load("navbar.html", function (response) {
+        $.get("navbar.html", function (response) {
             navbarHtml = response;
+            $("#navbar").html(navbarHtml);
             bindNavbarButton();
         });
     } else {
@@ -15,50 +16,46 @@ $(function () {
     }
 
     if (footerHtml === null) {
-        $("#footer").load("footer.html", function (response) {
+        $.get("footer.html", function (response) {
             footerHtml = response;
+            $("#footer").html(footerHtml);
         });
     } else {
         $("#footer").html(footerHtml);
     }
 });
 
-$(function () {
-    $("#navbar").load("navbar.html", function () {
-        // 绑定导航栏按钮和 logo 的点击事件
-        $('.navbar-button').off('click').click(function (e) {
-            e.preventDefault();  // 阻止默认的页面跳转行为
+function bindNavbarButton() {
+    // 绑定导航栏按钮和 logo 的点击事件
+    $('.navbar-button').off('click').click(function (e) {
+        e.preventDefault();  // 阻止默认的页面跳转行为
 
-            // 获取要加载的页面的 URL
-            var url = $(this).attr('href');
+        // 获取要加载的页面的 URL
+        var url = $(this).attr('href');
 
-            // 使用 AJAX 加载页面内容
-            $.get(url, function (data) {
-                // 将 data 转换为 jQuery 对象
-                var $data = $(jQuery.parseHTML(data));
+        // 使用 AJAX 加载页面内容
+        $.get(url, function (data) {
+            // 将 data 转换为 jQuery 对象
+            var $data = $(jQuery.parseHTML(data));
 
-                // 从加载的内容中提取 .main-content 的内容
-                var mainContent = $data.filter('.main-content').add($data.find('.main-content')).html();
+            // 从加载的内容中提取 .main-content 的内容
+            var mainContent = $data.filter('.main-content').add($data.find('.main-content')).html();
 
-                // 检查提取的内容是否为空
-                if (mainContent) {
-                    console.log('Content fetched successfully.');
-                } else {
-                    console.log('Failed to fetch content.');
-                }
-
+            // 检查提取的内容是否为空
+            if (mainContent) {
+                console.log('Content fetched successfully.');
                 // 将提取的内容插入到当前页面的 .main-content 元素中
                 $('.main-content').html(mainContent);
+            } else {
+                console.log('Failed to fetch content.');
+            }
 
-                // 更新页面标题
-                var title = $data.filter('title').text();
-                $('head title').text(title);
-            });
-
-            // 改变地址栏的 URL
-            history.pushState(null, null, url);
+            // 更新页面标题
+            var title = $data.filter('title').text();
+            $('head title').text(title);
         });
+
+        // 改变地址栏的 URL
+        history.pushState(null, null, url);
     });
-    // 加载页面尾部
-    $("#footer").load("footer.html");
-});
+}
